f73d4ad8256235eb3aefdc267a51fe571c7821a2
Marcus Baer <marcus.baer@publicispixelpark.de>
2017-03-02 12:18:17 +0100

    PWA setup

--- README.md
+++ README.md
@@ -17,3 +17,9 @@ This project is a playground for an application based on nuxt.js
 ## Deploy with Now.sh
 
     $ now
+
+## Tools
+
+- http://favicons.io/ to generate a favicon
+- http://realfavicongenerator.net/ to generate a manifest.json
+- https://manifest-validator.appspot.com/ for manifest validation
--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,9 @@ module.exports = {
     titleTemplate: '%s - Web app',
     meta: [
       { charset: 'utf-8' },
+      { name: 'apple-mobile-web-app-title', content: 'Example PWA' },
+      { name: 'application-name', content: 'Example PWA' },
+      { name: 'theme-color', content: '#686868' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Meta description' }
     ],
@@ -35,6 +38,11 @@ module.exports = {
       { src: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js' }
     ],
     link: [
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/manifest.json' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#006837' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto' }
     ]
   },
