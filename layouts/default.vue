ebbc238f679682eebaf80412d4778f0b099a7b03
Marcus Baer <marcus.baer@publicispixelpark.de>
2017-03-03 17:34:17 +0100

    nav layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,15 +4,11 @@
       <nuxt-link :to="path('/')" exact><img class="Header__Img" src="/android-chrome-192x192.png" /></nuxt-link>
       <h1>{{ $t('header.title') }}</h1>
       <nav class="Header__Menu">
-        <nuxt-link class="Header__Link" :to="path('/secret')">
-          {{ $t('links.secret') }}
-        </nuxt-link>
-        <nuxt-link class="Header__Link" v-if="$store.state.lang.lang === 'en'" :to="`/de` + $route.fullPath" active-class="none">
-          {{ $t('links.german') }}
-        </nuxt-link>
-        <nuxt-link class="Header__Link" v-else :to="$route.fullPath.replace(/^\/[^\/]+/, '')" active-class="none">
-          {{ $t('links.english') }}
-        </nuxt-link>
+        <nuxt-link class="Header__Link" :to="path('/secret')">{{ $t('links.secret') }}</nuxt-link>
+        <nuxt-link class="Header__Link" :to="path('/testpage')">{{ $t('links.test') }}</nuxt-link>
+
+        <nuxt-link class="Header__Link Header__Lang" v-if="$store.state.lang.lang === 'en'" :to="`/de` + $route.fullPath" active-class="none">{{ $t('links.german') }}</nuxt-link>
+        <nuxt-link class="Header__Link" v-else :to="$route.fullPath.replace(/^\/[^\/]+/, '')" active-class="none">{{ $t('links.english') }}</nuxt-link>
       </nav>
       <div class="onlinedot"></div>
     </header>
@@ -52,19 +48,23 @@ main {
 }
 
 .Header__Link {
-  font-size: 16px;
   color: #aaa;
-  border: 1px solid #aaa;
-  padding: 7px 12px;
-  text-transform: uppercase;
-  text-decoration: none;
-  border-radius: 5px;
-  margin-left: 10px;
+  margin-left: .5rem;
 }
 
+.Header__Link:active,
+.Header__Link:focus,
 .Header__Link:hover {
-  color: #2e2f30;
-  background-color: #fff;
+  color: orange;
+  text-decoration: none;
+}
+
+.Header__Lang {
+  background-color: #666;
+  color: #fff;
+  padding: .3rem;
+  text-transform: uppercase;
+  text-decoration: none;
 }
 
 .nuxt-link-active {
---
+++ pages/_lang/testpage.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="red">Hello {{ name }}!</h1>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Testpage'
+  },
+  data () {
+    return { name: 'world' }
+  }
+}
+</script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
