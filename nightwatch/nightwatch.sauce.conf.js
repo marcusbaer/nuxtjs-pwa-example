2b1b79176821fba7a528174594f7772ca7daf9eb
Marcus Baer <marcus.baer@publicispixelpark.de>
2017-03-06 16:26:52 +0100

    test with saucelabs

---
+++ nightwatch/nightwatch.sauce.conf.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "globals_path": "globals.js",
+  "output_folder": false,
+  "src_folders": ["specs"],
+  "test_settings": {
+    "default": {
+      "screenshots": {
+        "enabled": false,
+        "path": ""
+      },
+      "launch_url": process.env.LAUNCH_URL,
+      "selenium_host": "ondemand.saucelabs.com",
+      "selenium_port": 80,
+      "silent": true,
+      "username": process.env.SAUCE_USERNAME,
+      "access_key": process.env.SAUCE_ACCESS_KEY,
+      "globals": {
+        "waitForConditionTimeout": 10000
+      }
+    },
+    "chrome": {
+      "desiredCapabilities": {
+        "browserName": "chrome",
+        "platform": "OS X 10.11",
+        "version": "47"
+      }
+    },
+    "ie11": {
+      "desiredCapabilities": {
+        "browserName": "internet explorer",
+        "platform": "Windows 10",
+        "version": "11.0"
+      }
+    }
+  }
+}
--- nightwatch/specs/homepage.js
+++ nightwatch/specs/homepage.js
@@ -13,7 +13,7 @@ module.exports = {
     browser
       .pause(1000)
       .click('.Header__Lang')
-      .expect.element('.Header__Lang').text.to.equal('FRDE')
+      .expect.element('.Header__Lang').text.to.equal('DE')
     browser
       .pause(1000)
       .end()
